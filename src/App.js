2ac0b85e7a30dd5a5e5b2d891c2f2b56389e3efc
fajarsiagian <fajar7xx@gmail.com>
2020-02-27 21:20:25 +0700

    kiirm

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parentbox">
+      <FotoProduk/>
+      <ProdukInfo/>
     </div>
   );
 }
+function FotoProduk(){
+  return(
+    <div className="foto">
+        <img src="img/sepatu.jpg" alt=""/>
+      </div>
+  )
+}
 
+function ProdukInfo(){
+  const cat = "SNEAKER"
+  const title = "Sepatu Swag"
+  const price = "1.500.000"
+  const info = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ducimus. Labore minima dolor laudantium doloremque provident perferendis est, nam optio deserunt repellendus distinctio vitae error nemo, similique, numquam a quod?"
+
+  return(
+    <div className="deskripsi">
+        <p className="cate">{cat}</p>
+        <h1 className="title">{title}</h1>
+        <p className="price">Rp. {price}</p>
+        <p className="info"> {info} </p>
+      </div>
+  )
+}
 export default App;
